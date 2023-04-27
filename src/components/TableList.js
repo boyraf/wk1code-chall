5d083e4daddabd2657677f89a228dd8c55bc84dd
boyraf <raphael.kymotho@gmail.com>
2023-04-27 16:55:25 +0300

    commit

--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,31 +1,14 @@
-/*import React from "react";
-
-
-function TableList(date, description, category, amount){
-    return(
-        <li>
-            <h3>{date}</h3>
-            <h3>{description}</h3>
-            <h3>{category}</h3>
-            <h3>{amount}</h3>
-        </li>
-    )
-}
-
-export default TableList;*/
-
-
 import React from "react";
 
-function TableList(props) {
-  return (
-    <tr>
-      <td>{props.date}</td>
-      <td>{props.description}</td>
-      <td>{props.category}</td>
-      <td>{props.amount}</td>
-    </tr>
-  );
+function TableList({date, description, category, amount}) {
+    return(
+        <tr>
+            <td>{date}</td>
+            <td>{description}</td>
+            <td>{category}</td>
+            <td>{amount}</td>
+        </tr>
+    )
 }
 
 export default TableList;
